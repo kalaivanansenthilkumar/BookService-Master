ed19a6d6f837c499ee0934721654f847bbf75fe8
Senthilkumar <senthilkumar.ayyasamy@outlook.com>
2026-01-11 14:34:07 +0530

    BookService-CRUD Operations Checkin

--- BookService/Scripts/app.js
+++ BookService/Scripts/app.js
@@ -4,13 +4,14 @@
     self.error = ko.observable();
     self.detail = ko.observable();
     self.authors = ko.observableArray();
-    self.newBook = {
-        Author: ko.observable(),
-        Genre: ko.observable(),
-        Price: ko.observable(),
-        Title: ko.observable(),
-        Year: ko.observable()
-    }
+    self.Author= ko.observable();
+    self.Title = ko.observable();
+    self.Genre = ko.observable();
+    self.Year = ko.observable();
+    self.Price = ko.observable();
+    self.selectedAuthorId = ko.observable();
+    self.selectedBookId = ko.observable();
+    
 
     var booksUri = '/api/books/';
     var authorsUri = '/api/authors/';
@@ -48,25 +49,82 @@
 
 
     self.addBook = function (formElement) {
-        var book = {
-            AuthorId: self.newBook.Author().Id,
-            Genre: self.newBook.Genre(),
-            Price: self.newBook.Price(),
-            Title: self.newBook.Title(),
-            Year: self.newBook.Year()
+        var newBook = {
+            AuthorId: self.selectedAuthorId(),
+            Genre: self.Genre(),
+            Price: self.Price(),
+            Title: self.Title(),
+            Year: self.Year()
         };
 
-        ajaxHelper(booksUri, 'POST', book).done(function (item) {
+        ajaxHelper(booksUri, 'POST', newBook).done(function (item) {
+            self.clearFields();
+            alert('Book Added Successfully');         
             self.books.push(item);
         });
     }
-    //Delete Customer  
+    
+    self.getUpdateBook = function (book) {
+        self.selectedBookId(book.Id);
+        ajaxHelper(booksUri + book.Id, 'GET').done(function (data) {
+            self.selectedAuthorId(data.AuthorId);
+            self.Author(data.Author);
+            self.Title(data.Title);
+            self.Genre(data.Genre);
+            self.Year(data.Year);
+            self.Price(data.Price);
+
+            $('#Save').hide();
+            $('#Clear').hide();
+
+            $('#Update').show();
+            $('#Cancel').show();
+        });
+    }
+    //Update Book  
+    self.updateBook = function () {
+
+        var updateBook = {
+            Id: self.selectedBookId(),
+            AuthorId: self.selectedAuthorId(),
+            Genre: self.Genre(),
+            Price: self.Price(),
+            Title: self.Title(),
+            Year: self.Year()
+        };
+
+        ajaxHelper(booksUri + self.selectedBookId(), 'PUT', updateBook).done(function () {
+            alert('Book Updated Successfully !');
+            getAllBooks();
+            self.cancel();   
+        });
+    }
+    //Delete Book  
     self.deleteBook = function (book) {
         ajaxHelper(booksUri + book.Id, 'DELETE').done(function () {
             alert('Book Deleted Successfully');
             getAllBooks();
         })
 
+    }
+    // Clear Fields  
+    self.clearFields = function clearFields() {
+        self.selectedAuthorId('');
+        self.Title('');
+        self.Genre('');
+        self.Year('');
+        self.Price('');
+    }
+
+    self.cancel = function () {
+
+        self.clearFields();
+
+        $('#Save').show();
+        $('#Clear').show();
+
+        $('#Update').hide();
+        $('#Cancel').hide();
     }
     // Fetch the initial data.
     getAllBooks();
